44b5f29843139b14408439c39d1ee1b079daf4ec
dzikrialifa <dzikrialif99@gmail.com>
2020-04-09 22:29:34 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import BlogPost from './container/BlogPost.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BlogPost />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('content')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
